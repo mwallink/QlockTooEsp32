792a88492edee210c3db4e46ec8e21c572c01109
Martin Wallink <martin.wallink@gmail.com>
2022-04-07 13:56:17 +0200

    added password example

---
+++ include/wifipassword-example.h
@@ -0,0 +1,10 @@
+/*
+ * README
+ * Fill in the correct values below for ssid and password.
+ * Rename this file to "wifipassword.h".
+ * The file "wifipassword.h" is ignored by GIT.
+ */
+
+//Wifi credentials:
+const char* ssid = "network name";
+const char* password = "password";
